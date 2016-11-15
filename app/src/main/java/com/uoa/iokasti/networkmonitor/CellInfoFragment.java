fe42f760251d15c851446b02f3f1559e00fd0316
Maria Fava <sdi1000104@di.uoa.gr>
2016-11-15 05:34:24 +0200

    Location of connected cell on google maps.

--- app/src/main/java/com/uoa/iokasti/networkmonitor/CellInfoFragment.java
+++ app/src/main/java/com/uoa/iokasti/networkmonitor/CellInfoFragment.java
@@ -1,8 +1,13 @@
 package com.uoa.iokasti.networkmonitor;
 
 import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.telephony.CellInfo;
 import android.telephony.CellInfoCdma;
@@ -10,17 +15,33 @@ import android.telephony.CellInfoGsm;
 import android.telephony.CellInfoLte;
 import android.telephony.CellInfoWcdma;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class CellInfoFragment extends Fragment {
 
@@ -41,10 +62,25 @@ public class CellInfoFragment extends Fragment {
     private double latitude;
     private double longitude;
 
+    MapView mMapView;
+    private GoogleMap googleMap;
+
+    String openCellIdApiKey = "ef445193-fc82-482f-b199-9422b79a0e0a";
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cell_info, container, false);
         findViews(view);
+
+        /* map initialization */
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume(); // needed to get the map to display immediately
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         TelephonyManager telephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
         List<CellInfo> cellInfos = (List<CellInfo>) telephonyManager.getAllCellInfo();
@@ -92,6 +128,37 @@ public class CellInfoFragment extends Fragment {
         ratTextview.setText(rat);
         mccTextview.setText(String.valueOf(mcc));
         mncTextview.setText(String.valueOf(mnc));
+
+        final int finalCellId = cellId;
+        final int finalMcc = mcc;
+        final int finalMnc = mnc;
+        final int finalTac = tac;
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+
+                try {
+                    new setLatLongAsync().execute(finalCellId, finalMcc, finalMnc, finalTac).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+
+                // For dropping a marker at a point on the Map
+                LatLng cellLocation = new LatLng(latitude, longitude);
+                googleMap.addMarker(new MarkerOptions().position(cellLocation).title("Connected Cell #" + finalCellId).snippet("Location of currently used cell."));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(cellLocation).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
         return view;
     }
 
@@ -110,6 +177,30 @@ public class CellInfoFragment extends Fragment {
         mncTextview = (TextView) view.findViewById(R.id.mnc_textview);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
     private int getRssi(String CellSignalStrengthLte) {
         return Integer.valueOf(CellSignalStrengthLte.substring(CellSignalStrengthLte.indexOf("ss=") + 3, CellSignalStrengthLte.indexOf("rsrp") - 1));
     }
@@ -126,16 +217,30 @@ public class CellInfoFragment extends Fragment {
         return Integer.valueOf(CellSignalStrengthLte.substring(CellSignalStrengthLte.indexOf("cqi=") + 4, CellSignalStrengthLte.indexOf("ta") - 1));
     }
 
-    private void setLocation(int mcc, int mnc, int lac, int cellId, String radio) throws IOException {
-        String openCellIdApiKey = "ef445193-fc82-482f-b199-9422b79a0e0a";
-        String sUrl = "http://opencellid.org/cell/get?key=" + openCellIdApiKey + "&mcc=" +
-                String.valueOf(mcc) + "&mnc=" + String.valueOf(mnc) + "&lac=" + String.valueOf(lac) +
-                "&cellid=" + String.valueOf(cellId) + "&radio=" + radio + "&format=json";
 
-        URL url = new URL(sUrl);
-        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-        request.connect();
+    public class setLatLongAsync extends AsyncTask<Integer, Void, Void> {
+        @Override
+        protected Void doInBackground(Integer... params) {
+            int cellId = params[0];
+            int mcc = params[1];
+            int mnc = params[2];
+            int tac = params[3];
+            String sURL = String.format("http://opencellid.org/cell/get?key=ef445193-fc82-482f-b199-9422b79a0e0a&mcc=%d&mnc=%d&cellid=%d&lac=%d&format=json", mcc, mnc, cellId, tac);
+            try {
+                URL url = new URL(sURL);
+                HttpURLConnection request = (HttpURLConnection) url.openConnection();
+                request.connect();
 
-
+                // Convert to a JSON object
+                JsonParser jp = new JsonParser(); //from gson
+                JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+                JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+                latitude = rootobj.get("lat").getAsDouble();
+                longitude = rootobj.get("lon").getAsDouble();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
